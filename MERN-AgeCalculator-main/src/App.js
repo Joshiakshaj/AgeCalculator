d342d87a2f5490053e6bbce667252035b16ed409
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-11-10 11:12:40 +0530

    Add files via upload

---
+++ MERN-AgeCalculator-main/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import AgeCalc from './components/AgeCalc';
+
+function App() {
+  return (
+    <div>
+      <AgeCalc />
+    </div>
+  );
+}
+
+export default App;
